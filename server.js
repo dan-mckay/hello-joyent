deba2183d2c7dd0b6116567652a28a5d559d41bf
Dan McKay <daniel.mckay@feedhenry.com>
2013-09-28 23:13:02 +0100

    added server script

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Node running on Joyent Smart Machine</h1>');
+
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
